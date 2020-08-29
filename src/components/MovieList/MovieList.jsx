7d43019ee67b2459a595a7eedc64dc2047718761
Hans Accola <Hansolo6283@gmail.com>
2020-08-29 15:10:59 -0500

    created fetchMovies function in MovieList component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class MovieList extends Component {
+
+    componentDidMount() {
+        console.log('MovieList rendered...');
+    }
+
+    //sending dispatch to sagas to start axios request
+    fetchMovies = () => {
+        this.props.dispatch({ type: 'FETCH_MOVIES' });
+    }
+
     render() {
         return (
-            <h1>test</h1>
+            <>
+                <header>
+                    <h1>Movies In Library</h1>
+                </header>
+            </>
         )
     }
 }
